da881d7af92969780feca241b08eba225d4110a4
Devendra Verma <verma.devendra1102@gmail.com>
2017-02-14 18:31:07 +0530

    little progress

---
+++ analytics.js
@@ -0,0 +1,47 @@
+var analytics = (function (window, undefined) {
+  var analytics = {};
+
+  if (window.analytics) {
+    return;
+  }
+
+  window.analytics = analytics;
+
+  analytics.add = function (callback) {
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        console.log('Successfully connected to the backend server');
+      }
+    };
+    xhttp.open("GET", "http://192.168.1.124:3000/", true);
+    xhttp.send();
+    callback();
+  }
+
+  analytics.push = function (userInfo, callback) {
+    console.log('age is');
+    console.log(userInfo);
+    var xhttp = new XMLHttpRequest();
+    var post =
+      "name=" + encodeURIComponent(unescape(userInfo.name)) +
+      "&age=" + encodeURIComponent(unescape(userInfo.age)) +
+      "&email=" + encodeURIComponent(unescape(userInfo.email));
+    console.log('post is');
+    console.log(post);
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        console.log('Successfully pushed user');
+      }
+    };
+    xhttp.open("POST", "http://192.168.1.124:3000/insert", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send(post);
+    callback();
+  }
+
+  if (typeof window.analyticsReady === 'function') {
+    window.analyticsReady();
+  }
+})(window);
--- app.js
+++ app.js
@@ -1,12 +1,49 @@
 const express = require('express');
 const app = express();
+const cassandra = require('cassandra-driver');
+const client = new cassandra.Client({ contactPoints: ['127.0.0.1'], keyspace: 'test' });
+var bodyParser = require('body-parser');
 
-app.get('/', (req, res) => {
-  res.send('Test Success');
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
+/**bodyParser.json(options)
+ * Parses the text as JSON and exposes the resulting object on req.body.
+ */
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  const query = 'SELECT name, email FROM users WHERE email = ?';
+  client.execute(query, ['test@gmail.com'])
+    .then(result => res.json(result))
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+app.post('/insert', (req, res) => {
+  parseInt(req.body.age);
+  const query = 'insert into users(name, email, age) values(?, ?, ?)';
+  client.execute(query, [req.body.name, req.body.email, req.body.age], { prepare: true }).
+    then(result => {
+      res.send('User pushed successfully');
+      console.log('User pushed successfully');
+    })
+    .catch(err => {
+      console.log('err is');
+      console.log(err);
+    });
+});
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
 });
+
